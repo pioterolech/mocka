4e538b6f5857986fa6c3024f892a53cdfc24c1e6
Piotr Olechnowicz <piotr.olechnowicz@nordsec.com>
2022-10-03 11:27:25 +0300

    Change executableTarget to target

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "lyrebird",
     platforms: [
-        .macOS(.v12)
+        .macOS(.v10_12)
     ],
     products: [
         .library(name: "Mocka", targets: ["lyrebird"])
@@ -19,8 +19,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .executableTarget(name: "lyrebird", dependencies: [
-            // other dependencies
+        .target(name: "lyrebird", dependencies: [
             .product(name: "Vapor", package: "vapor"),
         ]),
         .testTarget(
